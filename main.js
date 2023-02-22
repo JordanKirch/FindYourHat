ff7b7990cdca63ebdb560392ac656d63bc1af77a
unknown <jkirchhoff123@gmail.com>
2023-02-21 23:35:58 -0700

    Completed checking for win or lose conditions and generateField

--- main.js
+++ main.js
@@ -9,22 +9,28 @@ var yCord = 0;
 var gameRuning = true;
 
 class Field {
-
+    //Creates the field
     constructor(field){
         this.field = field
     }
+
     //Starts the game and is the main loop for the program
     play(){
         while(gameRuning){
-            this.field.print();
+            this.print();
+            this.startingLocation();
+            this.userInput();
+
         }
 
     }
+
     //Prints current game board
     print(){
         console.log(this.field.join("\n"))
     }
 
+    //Finds the starting location of the '*' and records it's coordinates
     startingLocation(){
         for (var i = 0; i < this.field.length; i++){
             for(var j = 0; j < this.field.length; j++){
@@ -36,57 +42,56 @@ class Field {
         }
     }
 
+    //Takes given direction and moves the '*' in that direction.
     move(direction){
         //Moves Up by one
         if(direction == 'u' && yCord - 1 >= 0){
             yCord -= 1;
-            if(this.field[yCord][xCord] == hole){
-                this.lose();
-            }
-
+            this.checkLost();
+            this.checkWin();
             this.field[yCord][xCord] = pathCharacter;
         }
         
         //Moves Down by one
         else if(direction == 'd' && yCord + 1 <= this.field.length){
             yCord += 1;
-            if(this.field[yCord][xCord] == hole){
-                this.lose();
-            }
+            this.checkLost();
+            this.checkWin();
             this.field[yCord][xCord] = pathCharacter;
         }
         //Moves Right by one
-        else if(direction == 'r' && (this.field[yCord][xCord + 1] == fieldCharacter || this.field[yCord][xCord + 1] == hole)){
+        else if(direction == 'r' && xCord + 1 <= this.field.length){
             xCord += 1;
-            if(this.field[yCord][xCord] == hole){
-                this.lose();
-            }
+            this.checkLost();
+            this.checkWin();
             this.field[yCord][xCord] = pathCharacter;
         }
         //Moves Left by one
-        else if(direction == 'l' && (this.field[yCord][xCord - 1] == fieldCharacter || this.field[yCord][xCord - 1] == hole)){
+        else if(direction == 'l' && xCord - 1 >= 0){
             xCord -= 1;
-            if(this.field[yCord][xCord] == hole){
-                this.lose();
-            }
+            this.checkLost();
+            this.checkWin();
             this.field[yCord][xCord] = pathCharacter;
         }
         //Invalid move 
         else{
             console.log("You can not move there.");
-            // this.userInput();
         }
-
-        this.print();
     }
-
-    lose(){
-        gameRuning = false
-        console.log("You fell into a hole.");
+    //Checks if the user landed in a hole triggering a lost game
+    checkLost(){
+        if(this.field[yCord][xCord] == hole){
+            gameRuning = false;
+            console.log("You fell into a hole. You lose.");
+        }
+        
     }
-
+    //Checks if the user has reached the hat triggering a won game
     checkWin(){
-
+        if(this.field[yCord][xCord] == hat){
+            gameRuning = false;
+            console.log("You Reached the hat. You WIN!!!");
+        }
     }
     //Gets direction input from the user and then starts the move method in given direction
     userInput(){
@@ -96,12 +101,40 @@ class Field {
         }
         this.move(direction);
     }
-}
 
+    generateField(height, width, percent){
+        for(var i = 0; i < height; i++){
+            for(var j = 0; j < width; j++){
+                var hatPlaced = false;
+                var pathCharPlaced = false;
+                
+                //Place '*'
+                if(pathCharPlaced == false && Math.floor(Math.random(4) * 5) == 0){
+                    pathCharPlaced = true;
+                    this.field[i][j] = pathCharacter;
+                }
+                //place '^'
+                else if(hatPlaced == false && Math.floor(Math.random(4) * 5) == 0){
+                    hatPlaced = true;
+                    this.field[i][j] = hat;
+                }
+                //place 'O' given percentage
+                else if (Math.random() < percent/100){
+                    this.field[i][j] = hole;
+                }
+                else{
+                    this.field[i][j] = fieldCharacter;
+                }
+            }
+        }
+    }
+}
+//Creates field and starts game
+//Todo update the array size
 const myField = new Field([
-    ['*', '░', 'O'],
+    ['*', '░', '░'],
     ['░', 'O', '░'],
     ['░', '^', '░']
 ]);
-myField.print();
-myField.userInput();
+myField.generateField(2,2, 30);
+myField.play();
